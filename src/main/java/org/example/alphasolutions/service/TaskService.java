4aa8dba161dd21cbd43de87a7f6d365b6d4d36a1
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-05-09 15:55:48 +0200

    minor fixes

--- src/main/java/org/example/alphasolutions/service/TaskService.java
+++ src/main/java/org/example/alphasolutions/service/TaskService.java
@@ -15,30 +15,34 @@ public class TaskService {
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+    //----------create task----------
     public void createTask(Task task) {
         task.setTaskStatus("NOT_STARTED");
         task.setTaskTimeSpent(0);
         task.setTaskPriority("LOW_PRIORITY");
         taskRepository.save(task);
     }
-
+//----------get all tasks----------
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
-
+//----------get task by id----------
     public Task getTaskById(int taskID) {
         return taskRepository.findById(taskID);
     }
+    //----------get tasks by project id----------
     public List<Task> getTasksByProject(int projectID) {
         return taskRepository.findByProjectId(projectID);
     }
+    //----------get tasks by sub project id----------
     public List<Task> getTasksBySubProjectId(int subProjectID) {
         return taskRepository.findBySubProjectId(subProjectID);
     }
+    //----------update task----------
     public void updateTask(Task task, int taskID) {
         taskRepository.update(task, taskID);
     }
-
+    //----------delete task----------
     public void deleteTask(int taskID) {
         taskRepository.delete(taskID);
     }
