863f3799950c8a12144cd7fe5ca3975a34b10d58
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-05-12 20:22:16 +0200

    refactored to tasks for SP not project

--- src/main/java/org/example/alphasolutions/model/Task.java
+++ src/main/java/org/example/alphasolutions/model/Task.java
@@ -1,7 +1,6 @@
 package org.example.alphasolutions.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDate;
 
 public class Task {
@@ -16,17 +15,15 @@ public class Task {
     private int taskTimeSpent;
     private String taskStatus;
     private String taskPriority;
-    private int projectID;
     private int subProjectID;
 
     public Task() {
     }
 
-    //-------constructor for project tasks
-    public Task(int taskID, String taskName, String taskDescription, LocalDate taskStartDate, LocalDate taskDeadline,
-                int taskTimeEstimate, int taskTimeSpent, String taskStatus, String taskPriority,
-                int projectID) {
-
+    // Updated constructor for subproject tasks only
+    public Task(int taskID, String taskName, String taskDescription, LocalDate taskStartDate,
+                LocalDate taskDeadline, int taskTimeEstimate, int taskTimeSpent,
+                String taskStatus, String taskPriority, int subProjectID) {
         this.taskID = taskID;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
@@ -36,30 +33,10 @@ public class Task {
         this.taskTimeSpent = taskTimeSpent;
         this.taskStatus = taskStatus;
         this.taskPriority = taskPriority;
-        this.projectID = projectID;
-        this.subProjectID = 0;
-    }
-
-    //-------------constructor for subproject tasks
-    public Task(int taskID, String taskName, String taskDescription, LocalDate taskStartDate, LocalDate taskDeadline,
-                int taskTimeEstimate, int taskTimeSpent, String taskStatus, String taskPriority,
-                int projectID, int subProjectID) {
-
-        this.taskID = taskID;
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.taskStartDate = taskStartDate;
-        this.taskDeadline = taskDeadline;
-        this.taskTimeEstimate = taskTimeEstimate;
-        this.taskTimeSpent = taskTimeSpent;
-        this.taskStatus = taskStatus;
-        this.taskPriority = taskPriority;
-        this.projectID = projectID;
         this.subProjectID = subProjectID;
     }
 
-    //---------Getters and Setters
-    //----------------Task ID----------------
+    // Getters and Setters
     public int getTaskID() {
         return taskID;
     }
@@ -68,7 +45,6 @@ public class Task {
         this.taskID = taskID;
     }
 
-    //----------------Task Name----------------
     public String getTaskName() {
         return taskName;
     }
@@ -77,7 +53,6 @@ public class Task {
         this.taskName = taskName;
     }
 
-    //----------------Task Description----------------
     public String getTaskDescription() {
         return taskDescription;
     }
@@ -86,17 +61,14 @@ public class Task {
         this.taskDescription = taskDescription;
     }
 
-    //----------------Task Start Date----------------
     public LocalDate getTaskStartDate() {
         return taskStartDate;
     }
 
     public void setTaskStartDate(LocalDate taskStartDate) {
-
         this.taskStartDate = taskStartDate;
     }
 
-    //----------------Task Deadline----------------
     public LocalDate getTaskDeadline() {
         return taskDeadline;
     }
@@ -105,7 +77,6 @@ public class Task {
         this.taskDeadline = taskDeadline;
     }
 
-    //----------------Task Time Estimate----------------
     public int getTaskTimeEstimate() {
         return taskTimeEstimate;
     }
@@ -114,7 +85,6 @@ public class Task {
         this.taskTimeEstimate = taskTimeEstimate;
     }
 
-    //----------------Task time spent----------------
     public int getTaskTimeSpent() {
         return taskTimeSpent;
     }
@@ -123,9 +93,7 @@ public class Task {
         this.taskTimeSpent = taskTimeSpent;
     }
 
-    //----------------Task Status----------------
     public String getTaskStatus() {
-
         return taskStatus;
     }
 
@@ -133,7 +101,6 @@ public class Task {
         this.taskStatus = taskStatus;
     }
 
-    //----------------Task priority----------------
     public String getTaskPriority() {
         return taskPriority;
     }
@@ -142,16 +109,6 @@ public class Task {
         this.taskPriority = taskPriority;
     }
 
-    //----------------Project ID----------------
-    public int getProjectID() {
-        return projectID;
-    }
-
-    public void setProjectID(int projectID) {
-        this.projectID = projectID;
-    }
-
-    //----------------Sub Project ID----------------
     public int getSubProjectID() {
         return subProjectID;
     }
@@ -159,4 +116,5 @@ public class Task {
     public void setSubProjectID(int subProjectID) {
         this.subProjectID = subProjectID;
     }
+
 }
